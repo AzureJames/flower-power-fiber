c36ee1c73275e5862ba2fcc04b24c62099217d7f
Azure James G.M <77216235+AzureJames@users.noreply.github.com>
2022-12-29 12:53:02 -0500

    circ moves left repeats

--- src/RotatingBunny.js
+++ src/RotatingBunny.js
@@ -2,22 +2,28 @@ import { useCallback, useState } from "react";
 import { usePixiTicker } from "react-pixi-fiber/index.js";
 import Bunny from "./Bunny";
 
-let circleSpeed = 2
+let circleSpeed = 4
 // http://pixijs.io/examples/#/basics/basic.js
 function RotatingBunny(props) {
 
-  const [rotation, setRotation] = useState(800);
+  const [position, setPosition] = useState(800);
   const animate = useCallback((delta) => {
     // just for fun, let's rotate mr rabbit a little
     // delta is 1 if running at 100% performance
     // creates frame-independent tranformation
-    
-    setRotation((x) => 
-    x - circleSpeed * delta);
+
+
+    setPosition((x) => 
+    x - circleSpeed * delta
+    );
   }, []);
   usePixiTicker(animate);
 
-  return <Bunny {...props} x={rotation} />;
+  if(position < -150){setPosition((x) => 
+    x = 800
+  );}
+
+  return <Bunny {...props} x={position} />;
 }
 
 export default RotatingBunny;
