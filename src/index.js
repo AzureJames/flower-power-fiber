6e0e535a245733d401f0155949d71fa1d0f5af51
Azure James G.M <77216235+AzureJames@users.noreply.github.com>
2022-12-28 22:20:31 -0500

    basic works pixi

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createRoot } from "react-dom/client";
+import { Sprite, Stage, Texture } from "react-pixi-fiber/index.js";
+import bunny from "./bunny.png";
+import * as PIXI from "pixi.js"; 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
+function Bunny (props) {
+  return <Sprite texture={PIXI.Texture.from(bunny)} {...props} />;
+}
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  <>
+  <Stage options={{ backgroundColor: 0x10bb99, height: 600, width: 800 }}>
+    <Bunny x={200} y={200} />
+  </Stage>
+  </>,
+); 
