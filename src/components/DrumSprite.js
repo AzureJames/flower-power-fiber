6b4adfff7d658728d1807031d41529c714a7cf16
Azure James G.M <77216235+AzureJames@users.noreply.github.com>
2022-12-29 23:05:40 -0500

    sound

--- src/components/DrumSprite.js
+++ src/components/DrumSprite.js
@@ -4,18 +4,30 @@ import drumhit from "../img/drumhit.png";
 import * as PIXI from "pixi.js"; 
 import { useCallback, useState } from "react";
 //import { usePixiTicker } from "react-pixi-fiber/index.js";
+import drumsound from '../img/drumsound.mp3';
+import useSound from 'use-sound';
 
 export default function DrumSprite (props) {
   const [tex, setTex] = useState(drum);
-  
+  const [playSound] = useSound(drumsound)
 
   //this is broke. eventually i want a mouse interaction event timeout
   const Clicked = () => {
     
+playSound();
         setTex(drumhit); 
         setTimeout(() => setTex(drum), 50);
     }
   
+
+
+
+    
+    
+      
+      
+
+
 
 
     return (
